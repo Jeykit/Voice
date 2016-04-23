09306ced316738c5cee6af65f93d87efa11e79db
吴定明 <392071745@qq.com>
2016-04-22 22:46:22 -0700

    Initial Commit

---
+++ Voice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Voice
+//
+//  Created by Adaman on 4/22/16.
+//  Copyright © 2016 Adaman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Voice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Voice
+//
+//  Created by Adaman on 4/22/16.
+//  Copyright © 2016 Adaman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
